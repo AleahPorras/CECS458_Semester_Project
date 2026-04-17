20fd1d4cc8bb0281ca4ec542f2fc480a3de14f22
Kristen Masters <kristen.masters01@student.csulb.edu>
2026-04-17 00:31:19 -0700

    sperated into functions and created main function

--- spodify_link_strip.py
+++ spodify_link_strip.py
@@ -5,25 +5,39 @@ from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 
+def load_API():
+    load_dotenv("spodify.env")
 
-load_dotenv("spodify.env")
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri="http://127.0.0.1:3000",
+        scope="playlist-read-private playlist-read-collaborative"
+    ))
+    return sp
+        
+def fetch_playlist_ID(url): 
+    playlist_ID = url.split("/playlist/")[1].split("?")[0]
+    return playlist_ID
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=os.getenv("CLIENT_ID"),
-    client_secret=os.getenv("CLIENT_SECRET"),
-    redirect_uri="http://127.0.0.1:3000",
-    scope="playlist-read-private playlist-read-collaborative"
-))
+def strip_playlist(sp,playlist_ID):
+    results = sp.playlist_tracks(playlist_ID)
+    with open("playlist_data.txt", "w", encoding="utf-8") as f:
+        for item in results['items']:
+            track = item.get('track') or item.get('item')
+            if track:
+                line = f"{track['name']} - {track['artists'][0]['name']} - {track['album']['name']}"
+                f.write(line + "\n")
 
-playlist_ID = "6LCoMDQ5ZtvZJI8lLk0eVY"
-results = sp.playlist_tracks(playlist_ID)
+def main():
+    sp = load_API()
+    users_playlist = input("Please enter a spodify PUBLIC playlist: ")
+    playlist_ID = fetch_playlist_ID(users_playlist)
+    strip_playlist(sp,playlist_ID)
+
+    print("file is done being written")
+
+main()
 
 
-with open("playlist_data.txt", "w", encoding="utf-8") as f:
-    for item in results['items']:
-        track = item.get('track') or item.get('item')
-        if track:
-            line = f"{track['name']} - {track['artists'][0]['name']} - {track['album']['name']}"
-            f.write(line + "\n")
 
-print("Saved to playlist_data.txt")
